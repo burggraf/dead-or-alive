af4a0bd55bd3c8ece5e709f7ef3eff23179631ee
Mark Burggraf <markb@mantisbible.com>
2021-08-03 20:43:44 -0700

    get image

--- src/app/pages/game/game.page.ts
+++ src/app/pages/game/game.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { People } from 'models/Database';
 import { SupabaseService } from 'src/app/services/supabase.service';
+// import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-game',
@@ -10,7 +12,7 @@ import { SupabaseService } from 'src/app/services/supabase.service';
 export class GamePage implements OnInit {
 
   public person: People;
-  constructor(private supabaseService: SupabaseService) { }
+  constructor(private supabaseService: SupabaseService, public httpClient: HttpClient) { }
 
   ngOnInit() {
     this.getRandomPerson();
@@ -22,6 +24,13 @@ export class GamePage implements OnInit {
     else {
       if (data.length) {
         this.person = data[0];
+        console.log(`https://en.wikipedia.org/w/api.php?action=query&titles=${encodeURIComponent(this.person.name)}&prop=pageimages&format=json&pithumbsize=300`);
+        const subscription = this.httpClient.get(
+          `https://en.wikipedia.org/w/api.php?action=query&titles=${encodeURIComponent(this.person.name)}&prop=pageimages&format=json&pithumbsize=300`)
+          .subscribe((data) => {
+            console.log('data', data);
+          });
+        // https://en.wikipedia.org/w/api.php?action=query&titles=Justin+Long&prop=pageimages&format=json&pithumbsize=300
       } else {
         console.error('error getting person - array is empty');
       }
